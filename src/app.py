7ce5531c2edf9479eae43c11412804966c7bd8c6
fonzk <alfdiazr@hotmail.com>
2021-11-11 18:43:11 -0500

    En construccion esqueleto

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.secret_key = os.urandom(24)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("construccion.html")
 
 if __name__ == '__main__':
 	app.run()
