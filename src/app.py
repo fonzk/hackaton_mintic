ee2ce50ce205cf65f43a410d974eae7cb0e5c4a9
Reinaldo Omar <reymar.mendoza@gmail.com>
2021-11-11 22:02:50 -0500

    Cambio minimo en el punto de entrada

--- src/app.py
+++ src/app.py
@@ -5,18 +5,10 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 @app.route("/")
-def hello_world():
-    return render_template("construccion.html")
-
 @app.route("/home")
+@app.route("/index")
 def home():
     return render_template("index.html")
-
-# Reinaldo test
-@app.route("/api")
-def api():
-	return render_template("reinaldo.html")
-# Reinaldo test
 
 if __name__ == '__main__':
 	app.run()
